258cf48df20783c41bf0ed3a6ff3420f54b37625
MarcusThu <marcus.thuvesen@gmail.com>
2019-03-14 14:27:36 +0100

    upp

--- LineChartExample/ViewController.swift
+++ LineChartExample/ViewController.swift
@@ -11,13 +11,12 @@ import Charts // You need this line to be able to use Charts Library
 import CoreMotion
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var chtChart: LineChartView!
-    var on = false
+    var isDeviceMotionOn = false
     var numbers : [Double] = []
-    var accelerationCallTimer = Timer()
     let motionManager = CMMotionManager()
-    var point = 0
+    var currentNode = 0
     var maxValue : Double = 0
     var minValue : Double = 0
     var sumCloseToMaxPoints : Double = 0
@@ -43,74 +42,72 @@ class ViewController: UIViewController {
         startButtonOutlet.layer.cornerRadius = 15
         clearButtonOutlet.layer.cornerRadius = 15
     }
-   
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func updateGraph(){
         
         var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
         //here is the for loop
         for i in 0..<numbers.count {
-
+            
             let value = ChartDataEntry(x: Double(i), y: numbers[i]) // here we set the X and Y status in a data chart
             lineChartEntry.append(value) // here we add it to the data set
         }
-
-         let line1 = LineChartDataSet(values: lineChartEntry, label: "Number") //Here we convert lineChartEntry to a LineChartDataSet
-
+        
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number") //Here we convert lineChartEntry to a LineChartDataSet
+        
         line1.colors = [NSUIColor.blue] //Sets the colour to blue
         line1.circleRadius = 2
         let data = LineChartData() //This is the object that will be added to the chart
         data.addDataSet(line1) //Adds the line to the dataSet
         chtChart.data = data //finally - it adds the chart data to the chart and causes an update
-
-//        self.lineChartView.setVisibleXRange(minXRange: CGFloat(1), maxXRange: CGFloat(50))
-//        self.lineChartView.notifyDataSetChanged()
-//        self.lineChartView.moveViewToX(CGFloat(i))
+        
+        //        self.lineChartView.setVisibleXRange(minXRange: CGFloat(1), maxXRange: CGFloat(50))
+        //        self.lineChartView.notifyDataSetChanged()
+        //        self.lineChartView.moveViewToX(CGFloat(i))
         
         self.chtChart.setVisibleXRangeMinimum(1)
         self.chtChart.setVisibleXRangeMaximum(200)
         self.chtChart.notifyDataSetChanged()
-        self.chtChart.moveViewToX(Double(point))
-    
+        self.chtChart.moveViewToX(Double(currentNode))
         
-        point += 1
         
-        chtChart.chartDescription?.text = "My awesome chart" // Here we set the description for the graph
-
+        currentNode += 1
+        
+        chtChart.chartDescription?.text = "Seismograph" // Here we set the description for the graph
+        
     }
-
+    
     func startAccelerometer(){
         
-        motionManager.deviceMotionUpdateInterval = 1/20
+        motionManager.deviceMotionUpdateInterval = 1/20 //How many nodes per second?
         motionManager.startDeviceMotionUpdates(to: .main) { (motion, error) in
             
             if let motion = motion{
-              
+                
                 var x = motion.userAcceleration.x
                 var y = motion.userAcceleration.y
                 var z = motion.userAcceleration.z
                 
-                
                 x = round(100 * x) / 100
                 y = round(100 * y) / 100
                 z = round(100 * z) / 100
-                //print(z)
+        
+                //Omvandlar XYZ till positiva tal
                 if x < 0 {
                     x = abs(x)
                 }
-                
                 if y < 0 {
-                   y = abs(y)
+                    y = abs(y)
                 }
-                
                 if z < 0 {
                     z = abs(z)
                 }
-                
+        
                 self.acceleration = x+y+z
                 
                 
@@ -119,70 +116,45 @@ class ViewController: UIViewController {
                     //print("MAXVALUE = \(self.maxValue)")
                     self.maxValueLabel.text = "Max: " + String(self.maxValue)
                 }
-              /*  if z < self.minValue{
-                    self.minValue =
-                    //print("MINVALUE = \(self.minValue)")
-                    self.minValueLabel.text = "Min: " + String(self.minValue)
-                }*/
-                //Average Close To Max
                 
                 if self.maxValue > 0.3 && (z + 0.5) > self.maxValue{
-                     self.closeToMaxNodes += 1
-                     self.sumCloseToMaxPoints += Double(z)
+                    self.closeToMaxNodes += 1
+                    self.sumCloseToMaxPoints += Double(z)
                     
-                     self.averageCloseToMax = self.sumCloseToMaxPoints/Double(self.closeToMaxNodes)
+                    self.averageCloseToMax = self.sumCloseToMaxPoints/Double(self.closeToMaxNodes)
                     
-                     self.averageCloseToMax = round(self.averageCloseToMax * 100) / 100
-                     self.averageLabel.text = "Medel: " + String(self.averageCloseToMax) + "\n" + String(self.averageCloseToMin)
-                     print("MEDELVÄRDE" + String(self.averageCloseToMax))
+                    self.averageCloseToMax = round(self.averageCloseToMax * 100) / 100
+                    self.averageLabel.text = "Medel: " + String(self.averageCloseToMax) + "\n" + String(self.averageCloseToMin)
+                    print("MEDELVÄRDE" + String(self.averageCloseToMax))
                 }
                 
-                //Average Close To Min
-                
-               /* if self.minValue < -0.3 && (z - 0.5) < self.minValue{
-                    self.closeToMinNodes += 1
-                    self.sumCloseToMinPoints += Double(z)
-                    
-                    self.averageCloseToMin = self.sumCloseToMinPoints/Double(self.closeToMinNodes)
-                    
-                    self.averageCloseToMin = round(self.averageCloseToMin * 100) / 100
-                    self.averageLabel.text = "Medel: " + String(self.averageCloseToMax) + "\n" + String(self.averageCloseToMin)
-                    print("MEDELVÄRDE" + String(self.averageCloseToMax) + "\n" + String(self.averageCloseToMin))
-                }*/
-                
-               
-               
                 self.numbers.append(self.acceleration)
                 self.updateGraph()
                 
             }
         }
-        
     }
     
-    @IBAction func btnbutton(_ sender: Any) {
+    @IBAction func startBtn(_ sender: Any) {
         if motionManager.isDeviceMotionAvailable{
-            if on == false{
-                startTimer()
-                on = true
+            if isDeviceMotionOn == false{
+                isDeviceMotionOn = true
+                startAccelerometer()
                 buttonOutlet.setTitle("Stoppa", for: .normal)
             }
             else{
-                on = false
+                isDeviceMotionOn = false
                 motionManager.stopDeviceMotionUpdates()
-                stopTimer()
                 buttonOutlet.setTitle("Starta", for: .normal)
             }
         }
     }
     
-    @IBAction func clearChart(_ sender: UIButton) {
-        stopTimer()
+    @IBAction func clearChartBtn(_ sender: UIButton) {
         motionManager.stopDeviceMotionUpdates()
         numbers.removeAll()
         reloadInputViews()
-        startTimer()
-        on = true
+        isDeviceMotionOn = true
         buttonOutlet.setTitle("Stoppa", for: .normal)
         maxValue = 0
         minValue = 0
@@ -194,16 +166,6 @@ class ViewController: UIViewController {
         
     }
     
-    
-    func startTimer(){
-        
-       accelerationCallTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(startAccelerometer), userInfo: nil, repeats: true)
-    }
-    
-    func stopTimer(){
-        accelerationCallTimer.invalidate()
-    }
-
 }
 
 
